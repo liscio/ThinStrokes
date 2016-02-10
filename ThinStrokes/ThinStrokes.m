ea8624151650113660ca36ac74764ed6ddc182aa
Mike Ash <mike@mikeash.com>
2016-02-09 20:18:21 -0500

    Redo the override by swizzling on DVTLayoutManager instead of subclassing it. Also clean up a lot of plugin boilerplate.
    Playgrounds use a DVTLayoutManager subclass and the old code caused a crash because it substituted its own subclass instead, which didn't have the same methods. By swizzling DVTLayoutManager, the new code gets into playgrounds too, but it doesn't hit other NSLayoutManagers. It could subclass the playground layout manager instead, but this is more straightforward and achieves the same thing.
    
    All the notification shenanigans that led up to the dynamic subclassing appear to be unnecessary and have been simplified down to performing the swizzling from +pluginDidLoad:.

--- ThinStrokes/ThinStrokes.m
+++ ThinStrokes/ThinStrokes.m
@@ -16,115 +16,39 @@
 extern void CGContextSetFontSmoothingStyle(CGContextRef, int);
 extern int CGContextGetFontSmoothingStyle(CGContextRef);
 
-@interface ThinStrokes()
-
-@property(nonatomic, strong, readwrite) NSBundle *bundle;
-@property(nonatomic, assign, readwrite) BOOL useThinStrokes;
-@end
-
 @implementation ThinStrokes
 
-+ (instancetype)sharedPlugin {
-    return sharedPlugin;
-}
-
-- (id)initWithBundle:(NSBundle *)plugin
-{
-    if (self = [super init]) {
-        self.useThinStrokes = YES;
-        
-        // reference to plugin's bundle, for resource access
-        self.bundle = plugin;
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(didApplicationFinishLaunchingNotification:)
-                                                     name:NSApplicationDidFinishLaunchingNotification
-                                                   object:nil];
++ (void)pluginDidLoad:(NSBundle *)plugin {
+    Class DVTLayoutManager = NSClassFromString(@"DVTLayoutManager");
+    if(!DVTLayoutManager) {
+        NSLog(@"Couldn't find DVTLayoutManager, so ThinStrokes can't load. Reverting to normal font rendering.");
+        return;
     }
-    return self;
-}
-
-- (void)didApplicationFinishLaunchingNotification:(NSNotification*)noti {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSApplicationDidFinishLaunchingNotification object:nil];
     
-    [self replaceLayoutManagerForClass:NSClassFromString(@"DVTSourceTextView")];
-}
-
-static void ShowCGGlyphsButThinThisTime(id self, SEL _cmd, const CGGlyph *glyphs, const NSPoint *positions, NSUInteger count, NSFont *font, NSAffineTransform *matrix, NSDictionary *attributes, NSGraphicsContext *inContext) {
-
-    CGContextRef ctx = [inContext CGContext];
+    SEL showCGGlyphsSEL = @selector(showCGGlyphs:positions:count:font:matrix:attributes:inContext:);
     
-    // The below code—save for the swizzling—is copied nearly verbatim from
-    // iTerm. Here's the source: https://github.com/gnachman/iTerm2/blob/76fe643f505eb3a0eed5a8390c39325e3c22d179/sources/iTermTextDrawingHelper.m#L681
+    Method showCGGlyphsMethod = class_getInstanceMethod(DVTLayoutManager, showCGGlyphsSEL);
+    IMP oldIMP = method_getImplementation(showCGGlyphsMethod);
     
-    int savedFontSmoothingStyle = 0;
-    BOOL useThinStrokes = YES;
-    if (useThinStrokes) {
+    IMP newIMP = imp_implementationWithBlock(^(id self, const CGGlyph *glyphs, const NSPoint *positions, NSUInteger count, NSFont *font, NSAffineTransform *matrix, NSDictionary *attributes, NSGraphicsContext *inContext) {
+        
+        CGContextRef ctx = [inContext CGContext];
+        
+        // The below code—save for the swizzling—is copied nearly verbatim from
+        // iTerm. Here's the source: https://github.com/gnachman/iTerm2/blob/76fe643f505eb3a0eed5a8390c39325e3c22d179/sources/iTermTextDrawingHelper.m#L681
+        
         // This seems to be available at least on 10.8 and later. The only reference to it is in
         // WebKit. This causes text to render just a little lighter, which looks nicer.
-        savedFontSmoothingStyle = CGContextGetFontSmoothingStyle(ctx);
+        int savedFontSmoothingStyle = CGContextGetFontSmoothingStyle(ctx);
         CGContextSetFontSmoothingStyle(ctx, 16);
-    }
-    
-    CGContextSetTextDrawingMode(ctx, kCGTextFill);
-    
-    Class superclass = NSClassFromString(@"DVTLayoutManager");
-    IMP superIMP = [superclass instanceMethodForSelector: @selector(showCGGlyphs:positions:count:font:matrix:attributes:inContext:)];
-
-    ((void (*)(id, SEL, const CGGlyph *, const NSPoint *, NSUInteger, NSFont *, NSAffineTransform *, NSDictionary *, NSGraphicsContext *))superIMP)(self, _cmd, glyphs, positions, count, font, matrix, attributes, inContext);
-    
-    if (useThinStrokes) {
+        
+        CGContextSetTextDrawingMode(ctx, kCGTextFill);
+        
+        ((void (*)(id, SEL, const CGGlyph *, const NSPoint *, NSUInteger, NSFont *, NSAffineTransform *, NSDictionary *, NSGraphicsContext *))oldIMP)(self, showCGGlyphsSEL, glyphs, positions, count, font, matrix, attributes, inContext);
+        
         CGContextSetFontSmoothingStyle(ctx, savedFontSmoothingStyle);
-    }
-}
-
-// encapsulate code needed to override an existing method
-static void Override(Class c, SEL sel, void *fptr) {
-    RTMethod *superMethod = [[c superclass] rt_methodForSelector: sel];
-    RTMethod *newMethod = [RTMethod methodWithSelector: sel implementation: fptr signature: [superMethod signature]];
-    [c rt_addMethod: newMethod];
-}
-
-static Class createThinLayoutManager() {
-    Class DVTLayoutManager = NSClassFromString(@"DVTLayoutManager");
-    if(!DVTLayoutManager)
-        return nil;
-    
-    Class c = [DVTLayoutManager rt_createSubclassNamed: @"ThinLayoutManager"];
-    Override(c, @selector(showCGGlyphs:positions:count:font:matrix:attributes:inContext:), ShowCGGlyphsButThinThisTime);
-    
-    return c;
-}
-
-static Class ThinLayoutManager(void) {
-    static Class c = nil;
-    static dispatch_once_t pred;
-    dispatch_once(&pred, ^{
-        c = createThinLayoutManager();
     });
-    
-    return c;
-}
-
-- (void)replaceLayoutManagerForClass:(Class)clazz {
-    SEL selector = @selector(layoutManager);
-    Method m = class_getInstanceMethod(clazz, selector);
-    IMP oldImplementation = method_getImplementation(m);
-    IMP newImplementation = imp_implementationWithBlock((id)^(id SELF) {
-        id oldLM = ((id (*)(id, SEL))oldImplementation)(SELF, selector);
-        
-        if (![oldLM isKindOfClass:ThinLayoutManager()]) {
-            NSAssert([oldLM isKindOfClass:NSClassFromString(@"DVTLayoutManager")], @"Right?");
-            
-            [oldLM rt_setClass:ThinLayoutManager()];
-        }
-        
-        return oldLM;
-    });
-    method_setImplementation(m, newImplementation);
-}
-
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    method_setImplementation(showCGGlyphsMethod, newIMP);
 }
 
 @end
